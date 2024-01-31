24588373673c3d4f91b6dc002977eb8bde6b908b
Dieguin <Dieguin@DESKTOP-IGAS87S>
2024-01-31 20:11:35 +0100

    Creacion de dos operaciones mas
    -He creado una operacion que haga que se pueda añadir los alumnos
    creando un atributo nuevo que es el contador, y ademas he creado la
    operacion toString que me servira de mucho sobre todo para probar cosas.

--- src/Ejercicio6/Aula.java
+++ src/Ejercicio6/Aula.java
@@ -1,16 +1,35 @@
 package Ejercicio6;
 
+import java.util.Arrays;
+
 public class Aula {
 		
 private Alumno alumnos[];
+private int contador;
 	
 	public Aula() {
 		
 		alumnos = new Alumno[5];
-		
+		contador = 0;
 	}
 		
+	
+	public void añadirAlumnos(Alumno alumno) {
 		
+		if(contador<alumnos.length) {
+			
+			alumnos[contador] = alumno;
+			contador++;
+		}
+		
+	}
+	
+		
+	public String toString() {
+		return Arrays.toString(alumnos);
+	}
+
+
 	
 	
 }
