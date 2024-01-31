8ca52295a3e9522d9fb4318f9e28128025541e5f
Dieguin <Dieguin@DESKTOP-IGAS87S>
2024-01-31 21:36:26 +0100

    Modificacion de la clase Alumno
    - Creacion de los atributos, constructor parametrizado, setters y
    getters de los atributos y el toString.

--- src/Ejercicio6/Alumno.java
+++ src/Ejercicio6/Alumno.java
@@ -2,4 +2,63 @@ package Ejercicio6;
 
 public class Alumno {
 
+	private String nombre;
+	private String apellido;
+	private int telefono;
+	private Asignaturas calificaciones[];
+	
+	public Alumno(String nombre,String apellido) {
+		
+		this.nombre = nombre;
+		this.apellido = apellido;
+		calificaciones = new Asignaturas[4];
+		
+	}
+	
+	
+	
+	
+	public String toString() {
+		
+		return "["+nombre+","+apellido+","+calificaciones+"]";
+		
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+
+	
+	
+
+	
+
+
 }
