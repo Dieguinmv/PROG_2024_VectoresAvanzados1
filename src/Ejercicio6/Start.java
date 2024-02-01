079fe82a403aed540bc91c53cbb947bf8e18ffd1
Mañana <Maana@Aula8-03>
2024-02-01 11:30:29 +0100

    Modificacion del nombre de añadir Asignatura
    -He modificado el nombre de la operacion de añadir Asignaturas por
    addAsigntauras.

--- src/Ejercicio6/Start.java
+++ src/Ejercicio6/Start.java
@@ -21,21 +21,21 @@ public class Start {
 		Asignaturas asig4 = new Asignaturas("Quimica",5,3,1);
 		
 		
-		alumno1.añadirAsig(asig1);
-		alumno1.añadirAsig(asig2);
-		alumno1.añadirAsig(asig3);
-		alumno1.añadirAsig(asig4);
+		alumno1.addAsig(asig1);
+		alumno1.addAsig(asig2);
+		alumno1.addAsig(asig3);
+		alumno1.addAsig(asig4);
 
 	
-		Asignaturas asig5 = new Asignaturas("Lengua",5,8,9);
+		Asignaturas asig5 = new Asignaturas("Lengua",10,8,9);
 		Asignaturas asig6 = new Asignaturas("Mate",9,8,6);
 		Asignaturas asig7 = new Asignaturas("Fisica",2,8,5);
 		Asignaturas asig8 = new Asignaturas("Quimica",2,8,7);
 		
-		alumno2.añadirAsig(asig5);
-		alumno2.añadirAsig(asig6);
-		alumno2.añadirAsig(asig7);
-		alumno2.añadirAsig(asig8);
+		alumno2.addAsig(asig5);
+		alumno2.addAsig(asig6);
+		alumno2.addAsig(asig7);
+		alumno2.addAsig(asig8);
 
 		
 		System.out.println(aula);
