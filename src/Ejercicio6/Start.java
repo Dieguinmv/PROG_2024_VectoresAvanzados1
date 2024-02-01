63124c127f3d256de2a9e1d6b3ad801b25e82c50
Mañana <Maana@Aula8-03>
2024-02-01 10:55:26 +0100

    MODIFICACION DEL START
    -He modificado el Start creando dos alumnos completos.

--- src/Ejercicio6/Start.java
+++ src/Ejercicio6/Start.java
@@ -5,6 +5,40 @@ public class Start {
 	public static void main(String[] args) {
 	
 		
+		Aula aula = new Aula();
+		
+		Alumno alumno1 = new Alumno("Diego","Montes");
+		Alumno alumno2 = new Alumno("Lorena","Rodriguez");
+		
+		aula.añadirAlumnos(alumno1);
+		aula.añadirAlumnos(alumno2);
+		
+		
+		
+		Asignaturas asig1 = new Asignaturas("Lengua",10,3,1);
+		Asignaturas asig2 = new Asignaturas("Mate",9,5,2);
+		Asignaturas asig3 = new Asignaturas("Fisica",2,5,7);
+		Asignaturas asig4 = new Asignaturas("Quimica",5,3,1);
+		
+		
+		alumno1.añadirAsig(asig1);
+		alumno1.añadirAsig(asig2);
+		alumno1.añadirAsig(asig3);
+		alumno1.añadirAsig(asig4);
+
+	
+		Asignaturas asig5 = new Asignaturas("Lengua",5,8,9);
+		Asignaturas asig6 = new Asignaturas("Mate",9,8,6);
+		Asignaturas asig7 = new Asignaturas("Fisica",2,8,5);
+		Asignaturas asig8 = new Asignaturas("Quimica",2,8,7);
+		
+		alumno2.añadirAsig(asig5);
+		alumno2.añadirAsig(asig6);
+		alumno2.añadirAsig(asig7);
+		alumno2.añadirAsig(asig8);
+
+		
+		System.out.println(aula);
 		
 		
 	}
